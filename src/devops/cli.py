47d48e8184c4c19103153f90bd172f44a4016f53
Charan, Varun <varun.charan@barco.com>
2022-07-21 05:04:56 +0100

    Added vault functionality to pyaction

--- src/devops/cli.py
+++ src/devops/cli.py
@@ -14,6 +14,7 @@ import click_option_group
 
 import devops.cheeseshop
 import devops.execute
+import devops.vault
 import devops.logconfig
 
 # ----------------------------------------------------------------------------
@@ -71,3 +72,60 @@ def execute(env, command):
     """
 
     devops.execute.execute(command, env)
+
+# ----------------------------------------------------------------------------
+# Vault
+# ----------------------------------------------------------------------------
+@cli.group()
+def vault():
+    """
+        Vault commands.
+
+        Assumes VAULT_ADDR is in the env.
+
+        If VAULT_SA_TOKEN and VAULT_ROLE_NAME are set in env, authentication with k8s-prd03 mount point will be attempted.
+
+        If VAULT_SA_TOKEN, VAULT_ROLE_NAME and VAULT_MOUNT_POINT are set in env, they are used for authentication.
+
+        If called without options, assumes either VAULT_TOKEN is in the env or ~/.vault_token holds a valid token.
+
+        See subcommands' help messages for option details.
+
+    """
+
+
+@vault.command('get')
+@click.argument('secret_path')
+@click.argument('secret_name')
+def get_vault_secret(secret_path, secret_name):
+    """
+        Get a Vault secret.
+
+        Assumes VAULT_ADDR is in the env.
+
+        If VAULT_SA_TOKEN and VAULT_ROLE_NAME are set in env, authentication with k8s-prd03 mount point will be attempted.
+
+        If VAULT_SA_TOKEN, VAULT_ROLE_NAME and VAULT_MOUNT_POINT are set in env, they are used for authentication.
+
+        If called without options, assumes either VAULT_TOKEN is in the env or ~/.vault_token holds a valid token.
+    """
+
+    return click.echo(devops.vault.get_secret(
+        secret_path,
+        secret_name))
+
+
+@vault.command('get-login-token')
+def get_login_token():
+    """
+        Get a Vault login token.
+
+        Assumes VAULT_ADDR is in the env.
+
+        If VAULT_SA_TOKEN and VAULT_ROLE_NAME are set in env, authentication with k8s-prdtwo mount point will be attempted.
+
+        If called without options, assumes either VAULT_TOKEN is in the env or ~/.vault_token holds a valid token.
+    """
+
+    return click.echo(devops.vault.get_login_token())
+
